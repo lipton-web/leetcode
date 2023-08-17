c4ecf1d12e4fa6e9e8c04dfd5a268668acf3d379
Jin-Sik Choi <82028756+lipton-web@users.noreply.github.com>
2023-08-17 09:42:36 +0900

    Time: 74 ms (97.47%), Space: 46 MB (17.47%) - LeetHub

---
+++ 2812-find-the-maximum-achievable-number/2812-find-the-maximum-achievable-number.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} num
+ * @param {number} t
+ * @return {number}
+ */
+var theMaximumAchievableX = function(num, t) {
+    const sum = num + t
+    const x = sum + t
+    return x
+};
