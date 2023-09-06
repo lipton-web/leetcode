72f8918472686082f6cea1dfbe4d41be6f8a235b
Jin-Sik Choi <82028756+lipton-web@users.noreply.github.com>
2023-09-06 12:19:31 +0900

    Time: 110 ms (95.67%), Space: 97 MB (15.89%) - LeetHub

---
+++ 0975-range-sum-of-bst/0975-range-sum-of-bst.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var rangeSumBST = function(root, low, high) {
+    let sum = 0
+    
+    if(root){
+        sum += (root.val >= low && root.val <= high)? root.val : 0;
+        sum += rangeSumBST(root.left, low, high);
+        sum += rangeSumBST(root.right, low, high);  
+    }
+
+    return sum
+};
