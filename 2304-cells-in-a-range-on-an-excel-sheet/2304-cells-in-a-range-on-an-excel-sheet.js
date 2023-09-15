947abcc3e7127e6bcb20cad927b0db804a01109c
Jin-Sik Choi <82028756+lipton-web@users.noreply.github.com>
2023-09-15 10:24:59 +0900

    Time: 66 ms (49.28%), Space: 44.6 MB (60.87%) - LeetHub

--- 2304-cells-in-a-range-on-an-excel-sheet/2304-cells-in-a-range-on-an-excel-sheet.js
+++ 2304-cells-in-a-range-on-an-excel-sheet/2304-cells-in-a-range-on-an-excel-sheet.js
@@ -3,15 +3,15 @@
  * @return {string[]}
  */
 var cellsInRange = function(s) {
-    const split = s.split("")
-    // console.log(split)
+    const split = s.split(":")
+    console.log(split)
     // console.log("ASCII", 'f'.charCodeAt(0))
 
     const answer = []
 
-    for(let i = split[0].charCodeAt(0); i <= split[3].charCodeAt(0); i++) {
+    for(let i = split[0][0].charCodeAt(0); i <= split[1][0].charCodeAt(0); i++) {
         // console.log(String.fromCharCode(i));
-        for(let j = split[1]; j <= split[4]; j++) {
+        for(let j = split[0][1]; j <= split[1][1]; j++) {
             answer.push(String.fromCharCode(i) + j)
         }
     }
