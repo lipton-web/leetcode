771c456c0523a2791aaeaf2f312cdbcbda4c1ad1
Jin-Sik Choi <82028756+lipton-web@users.noreply.github.com>
2023-10-12 10:19:20 +0900

    Time: 55 ms (46.75%), Space: 43.7 MB (55.31%) - LeetHub

--- 1677-matrix-diagonal-sum/1677-matrix-diagonal-sum.js
+++ 1677-matrix-diagonal-sum/1677-matrix-diagonal-sum.js
@@ -6,10 +6,12 @@ var diagonalSum = function(mat) {
     const size = mat[0].length
 
     let sum = 0
-    mat.forEach((item, idx, arr) => {
+    mat.forEach((item, idx) => {
         if(size%2 === 0) {
+            // 크기가 짝수일 때
             sum += item[idx] + item[size-1-idx]
         } else {
+            // 크기가 홀수일 때
             const harf = Math.floor(size/2)
             if(Math.floor(size/2) === idx) {
                 sum += item[harf]
