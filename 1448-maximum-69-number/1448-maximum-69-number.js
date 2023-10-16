89dcc9d6293f148d964cb56735f9c82ec8b9222d
Jin-Sik Choi <82028756+lipton-web@users.noreply.github.com>
2023-10-16 19:23:22 +0900

    Time: 56 ms (26.4%), Space: 42.1 MB (21.26%) - LeetHub

--- 1448-maximum-69-number/1448-maximum-69-number.js
+++ 1448-maximum-69-number/1448-maximum-69-number.js
@@ -5,8 +5,8 @@
 var maximum69Number  = function(num) {
     const arr = [...String(num)]
     for(let i=0; i<arr.length; i++){
-        if(arr[i]==6){
-            arr[i] = '9';
+        if(arr[i] === "6"){
+            arr[i] = "9";
             break;
         }
     }
