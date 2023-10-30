78147001015a42bb896c53952b5cd81bd9c626b8
Jin-Sik Choi <82028756+lipton-web@users.noreply.github.com>
2023-10-30 09:44:23 +0900

    Time: 84 ms (60.16%), Space: 49.2 MB (26.65%) - LeetHub

---
+++ 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[][]}
+ */
+var findDifference = function(nums1, nums2) {
+    const set1 = new Set(nums1);
+    const set2 = new Set(nums2);
+    const differ1 = new Set([...set1].filter((value) => !set2.has(value)));
+    const differ2 = new Set([...set2].filter((value) => !set1.has(value)));
+
+    return [[...differ1], [...differ2]];
+};
