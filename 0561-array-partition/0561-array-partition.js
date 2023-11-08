666ba065bb546be0cb6ccc1ab9998bb33639dbe4
Jin-Sik Choi <82028756+lipton-web@users.noreply.github.com>
2023-11-08 10:21:42 +0900

    Time: 108 ms (20.71%), Space: 47.4 MB (54.16%) - LeetHub

--- 0561-array-partition/0561-array-partition.js
+++ 0561-array-partition/0561-array-partition.js
@@ -4,12 +4,10 @@
  */
 var arrayPairSum = function(nums) {
     let result = 0;
-    console.log(nums.sort((a, b) => a - b))
     const numsSort = nums.sort((a, b) => a - b);
 
     numsSort.forEach((item, idx) => {
         if(idx % 2 === 0) return result += item;
     })
-    console.log("result :", result)
     return result;
 };
