7bef3afbd29703725f2c8b3cf1b5ee72fef61ae1
Jin-Sik Choi <82028756+lipton-web@users.noreply.github.com>
2023-12-01 09:43:25 +0900

    Time: 100 ms (82.85%), Space: 47.2 MB (53.51%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    const roman = {I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M:1000};
+
+    let result = 0;
+
+    for(let i = 0; s.length > i; i++) {
+        const cur = roman[s[i]];
+        const next = roman[s[i+1]];
+        if(next > cur) {
+            result += next - cur;
+            i++;
+        } else {
+            result += cur;
+        }
+    }
+    return result;
+
+};
